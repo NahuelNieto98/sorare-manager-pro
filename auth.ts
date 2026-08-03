327749c0bf39b640732a9faa22b8ba8eea785d08
Nahuel Nieto <nahueelnieto98@gmail.com>
2026-08-03 12:52:21 +0200

    Fix auth production configuration

--- auth.ts
+++ auth.ts
@@ -3,37 +3,60 @@ import Google from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/lib/prisma";
 
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
+
   debug: true,
+
+  trustHost: true,
 
   adapter: PrismaAdapter(prisma),
 
+
   providers: [
+
     Google({
+
       clientId: process.env.AUTH_GOOGLE_ID!,
+
       clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+
     }),
+
   ],
 
+
+
   session: {
+
     strategy: "database",
+
   },
 
-  pages: {
-    error: "/auth/error",
-  },
+
 
   logger: {
+
     error(error: Error) {
+
       console.error("[AUTH ERROR]", error);
+
     },
+
 
     warn(code: string) {
+
       console.warn("[AUTH WARN]", code);
+
     },
 
+
     debug(code: string, metadata?: unknown) {
+
       console.log("[AUTH DEBUG]", code, metadata);
+
     },
+
   },
+
 });
