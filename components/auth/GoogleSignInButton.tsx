5e05bcebe3fcaf8a729fb6140e4793283a4a2e4f
Nahuel Nieto <nahueelnieto98@gmail.com>
2026-08-03 12:38:00 +0200

    Improve Google login button

--- components/auth/GoogleSignInButton.tsx
+++ components/auth/GoogleSignInButton.tsx
@@ -1,14 +1,62 @@
 "use client";
 
 import { signIn } from "next-auth/react";
+import { useState } from "react";
 
 export default function GoogleSignInButton() {
+
+  const [loading, setLoading] = useState(false);
+
+
+  async function handleLogin() {
+
+    try {
+
+      setLoading(true);
+
+
+      await signIn("google", {
+        callbackUrl: "/dashboard",
+      });
+
+
+    } catch (error) {
+
+      console.error("Error login Google:", error);
+      setLoading(false);
+
+    }
+
+  }
+
+
+
   return (
+
     <button
-      onClick={() => signIn("google", { callbackUrl: "/dashboard" })}
-      className="rounded-xl bg-purple-600 px-8 py-4 text-lg font-semibold transition hover:bg-purple-500"
+      onClick={handleLogin}
+      disabled={loading}
+      className="
+      rounded-xl
+      bg-purple-600
+      px-8
+      py-4
+      text-lg
+      font-semibold
+      transition
+      hover:bg-purple-500
+      disabled:opacity-50
+      "
     >
-      🚀 Prueba gratis 14 días
+
+      {loading
+        ? "Conectando..."
+        : "🚀 Prueba gratis 14 días"
+      }
+
+
     </button>
+
   );
+
 }
