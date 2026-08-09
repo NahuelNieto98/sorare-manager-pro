c9efbf54469a85168966921df5b58d48745ebe8c
Nahuel Nieto <nahueelnieto98@gmail.com>
2026-08-09 09:47:19 +0200

    Fix Sorare player scores

--- lib/sorare/getUserCards.ts
+++ lib/sorare/getUserCards.ts
@@ -34,13 +34,30 @@ slug
 
 cardPositions
 
-l5Score
 
-l10Score
+l5: averageScore(
+type: LAST_FIVE_SO5_AVERAGE_SCORE
+)
 
-l15Score
 
-l40Score
+l10: averageScore(
+type: LAST_TEN_PLAYED_SO5_AVERAGE_SCORE
+)
+
+
+l15: averageScore(
+type: LAST_FIFTEEN_SO5_AVERAGE_SCORE
+)
+
+
+l40: averageScore(
+type: LAST_FORTY_SO5_AVERAGE_SCORE
+)
+
+
+aa15: averageScore(
+type: LAST_FIFTEEN_AVERAGE_ALL_AROUND_SCORE
+)
 
 }
 
@@ -97,15 +114,11 @@ while(attempts < 3){
 try{
 
 return await sorareRequest(
-
 GET_USER_CARDS,
-
 {
 after,
 },
-
 accessToken
-
 );
 
 
@@ -113,15 +126,10 @@ accessToken
 
 attempts++;
 
-
 console.log(
-
 `⚠️ Sorare bloqueado intento ${attempts}/3`,
-
 error.message
-
 );
-
 
 
 if(attempts >= 3){
@@ -129,7 +137,6 @@ if(attempts >= 3){
 throw error;
 
 }
-
 
 
 await sleep(
@@ -146,10 +153,7 @@ throw new Error(
 "Error obteniendo cartas Sorare"
 );
 
-
 }
-
-
 
 
 
@@ -158,14 +162,13 @@ export async function getUserCards(
 accessToken:string
 ){
 
-
 console.log(
 "🔥 OBTENIENDO GALERÍA CON OAUTH"
 );
 
 
 
-let allCards:any[] = [];
+let allCards:any = [];
 
 let after:string|null = null;
 
@@ -174,8 +177,6 @@ let previousCursor:string|null = null;
 let page = 1;
 
 let hasNextPage = true;
-
-
 
 
 
@@ -189,14 +190,9 @@ console.log(
 
 
 const data = await requestWithRetry(
-
 accessToken,
-
 after
-
 );
-
-
 
 
 
@@ -211,10 +207,7 @@ throw new Error(
 data.errors[0].message
 );
 
-
 }
-
-
 
 
 
@@ -223,67 +216,51 @@ data.data.currentUser.cards.nodes;
 
 
 
-
-
 if(page === 1){
 
 
 console.log(
-
 "🔎 PRIMERA CARTA DEBUG:",
-
 JSON.stringify(
-
 cards[0],
-
 null,
-
 2
-
 )
-
 );
 
 
 
 console.log(
-
 "📊 SCORES DEBUG:",
-
 JSON.stringify(
-
 {
-
 player:
 cards[0]?.anyPlayer?.displayName,
 
 l5:
-cards[0]?.anyPlayer?.l5Score,
+cards[0]?.anyPlayer?.l5,
 
 l10:
-cards[0]?.anyPlayer?.l10Score,
+cards[0]?.anyPlayer?.l10,
 
 l15:
-cards[0]?.anyPlayer?.l15Score,
+cards[0]?.anyPlayer?.l15,
 
 l40:
-cards[0]?.anyPlayer?.l40Score,
+cards[0]?.anyPlayer?.l40,
+
+aa15:
+cards[0]?.anyPlayer?.aa15,
 
 },
-
 null,
-
 2
-
 )
-
 );
 
 
 
 }
-
-
 
 
 
@@ -293,21 +270,13 @@ allCards.push(
 
 
 
-
-
 hasNextPage =
-
 data.data.currentUser.cards.pageInfo.hasNextPage;
 
 
 
-
-
 after =
-
 data.data.currentUser.cards.pageInfo.endCursor;
-
-
 
 
 
@@ -331,8 +300,6 @@ page++;
 
 
 
-
-
 if(hasNextPage){
 
 await sleep(1500);
@@ -340,158 +307,116 @@ await sleep(1500);
 }
 
 
-
 }
 
 
 
-
-
 console.log(
-
 "🔥 TOTAL CARTAS:",
-
 allCards.length
-
 );
 
 
 
-
-
-
-
 return allCards.map(
-
 (card:any)=>({
 
 
 assetId:
-
 card.assetId,
 
 
-
 slug:
-
 card.slug,
 
 
-
 season:
-
 card.seasonYear,
 
 
-
 rarity:
-
 card.displayRarity?.toLowerCase()
 ??
 "limited",
 
 
-
 marketValue:
-
 null,
-
-
-
 
 
 player:{
 
 
-
 displayName:
-
 card.anyPlayer?.displayName
 ??
 "Desconocido",
 
 
-
-
 slug:
-
 card.anyPlayer?.slug
 ??
 null,
 
 
-
-
 position:
-
 card.anyPlayer?.cardPositions?.[0]
 ??
 null,
 
 
-
-
 l5Score:
-
-card.anyPlayer?.l5Score
+card.anyPlayer?.l5
 ??
 null,
-
-
 
 
 l10Score:
-
-card.anyPlayer?.l10Score
+card.anyPlayer?.l10
 ??
 null,
-
-
 
 
 l15Score:
-
-card.anyPlayer?.l15Score
+card.anyPlayer?.l15
 ??
 null,
-
-
 
 
 l40Score:
-
-card.anyPlayer?.l40Score
+card.anyPlayer?.l40
 ??
 null,
 
 
+averageScore:
+card.anyPlayer?.l15
+??
+null,
+
+
+aaScore:
+card.anyPlayer?.aa15
+??
+null,
 
 
 club:
-
 card.anyTeam?.name
 ??
 null,
 
 
-
-
 pictureUrl:
-
 card.pictureUrl
 ??
 null,
 
 
-
 },
 
 
-
-
-
 pictureUrl:
-
 card.pictureUrl
 ??
 null,
@@ -499,8 +424,6 @@ null,
 
 })
 
-
 );
-
 
 }
