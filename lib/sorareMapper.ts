47de8574323f78516ecc528836ff6aee340bdc24
Nahuel Nieto <nahueelnieto98@gmail.com>
2026-08-09 10:00:08 +0200

    Fix AA score mapping

--- lib/sorareMapper.ts
+++ lib/sorareMapper.ts
@@ -1,26 +1,49 @@
 import { SorareCard } from "./types";
 
+
 export function mapSorareCard(card: any): SorareCard {
-  return {
-    id: card.assetId ?? "",
-    assetId: card.assetId ?? "",
 
-    slug: card.slug ?? "",
+return {
 
-    playerName: card.player?.displayName ?? "Jugador",
+id: card.assetId ?? "",
 
-    season: card.season ?? 0,
+assetId: card.assetId ?? "",
 
-    scarcity: card.rarity ?? "limited",
 
-    club: card.player?.activeClub?.name ?? null,
+slug: card.slug ?? "",
 
-    position: card.player?.position ?? null,
 
-    pictureUrl: card.player?.pictureUrl ?? null,
+playerName:
+card.player?.displayName ?? "Jugador",
 
-    averageScore: null,
 
-    marketValue: null,
-  };
+season:
+card.season ?? 0,
+
+
+scarcity:
+card.rarity ?? "limited",
+
+
+club:
+card.player?.club ?? null,
+
+
+position:
+card.player?.position ?? null,
+
+
+pictureUrl:
+card.pictureUrl ?? null,
+
+
+averageScore:
+card.player?.averageScore ?? null,
+
+
+marketValue:
+card.marketValue ?? null,
+
+};
+
 }
